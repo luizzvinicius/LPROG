73b180146397b9a006bd38790307ea7adcc6de79
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-07 00:33:52 -0300

    Feature: add media

--- atividade_5/java/src/Media.java
+++ atividade_5/java/src/Media.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Media {
     public static void main(String[] args) {
-        
+        try (var scan = new Scanner(System.in)) {
+            var notasComPeso = new ArrayList<Double>();
+            var pesos = new ArrayList<Double>();
+            final int MARKS = 3;
+            for (var i = 1; i <= MARKS; i++) {
+                System.out.printf("Digite a %d° nota: ", i);
+                var nota = scan.nextDouble();
+                System.out.printf("Digite o peso dessa nota: ");
+                var peso = scan.nextDouble();
+                notasComPeso.add(nota * peso);
+                pesos.add(peso);
+            }
+            var somaNotas = notasComPeso.stream().reduce(0d, Double::sum);
+            var somaPesos = pesos.stream().reduce(0d, Double::sum);
+            System.out.printf("A média Ponderada é: %.2f", somaNotas / somaPesos);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
