c7ee816e9a954b19d0370e4738c5e73453fe41ff
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-06 23:54:51 -0300

    Feature: add maioridade

--- atividade_5/java/src/Maioridade.java
+++ atividade_5/java/src/Maioridade.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Maioridade {
     public static void main(String[] args) {
-        
+        try (var scan = new Scanner(System.in)) {
+            System.out.print("Digite sua idade e verifique se é maior de idade: ");
+            int idade = scan.nextInt();
+            System.out.println(idade < 18 ? "Menor de idade" : "Maior de idade");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
