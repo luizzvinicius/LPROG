878f286e9550771df0d521c3b8c3426c8cb8bd3f
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-06 23:51:53 -0300

    Feature: add par/impar

--- atividade_5/java/src/Par.java
+++ atividade_5/java/src/Par.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Par {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        try (var scan = new Scanner(System.in)) {
+            System.out.print("Digite um número para verificar se é par ou ímpar: ");
+            int num = scan.nextInt();
+            System.out.println(num % 2 == 0 ? "Par" : "Ímpar"); 
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
