d4540a90f2884ac1615e6129322428e7de042d25
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-07 00:33:59 -0300

    Feature: add temperatura

--- atividade_5/java/src/Temperatura.java
+++ atividade_5/java/src/Temperatura.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Temperatura {
     public static void main(String[] args) {
-        
+        try (var scan = new Scanner(System.in)) {
+            System.out.print("Digite uma temperatura em C°: ");
+            var tempC = scan.nextDouble();
+            System.out.printf("%.1f em C° corresponde a %.1f F°%n", tempC, tempC * 1.8 + 32);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
