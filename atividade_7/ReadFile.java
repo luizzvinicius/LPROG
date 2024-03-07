d6941cf905fc1abf471545ced5bb3d3a7b99674b
Luiz Vinícius <viniciusadr244@gmail.com>
2024-03-07 15:56:14 -0300

    refactor

--- atividade_7/ReadFile.java
+++ atividade_7/ReadFile.java
@@ -5,22 +5,18 @@ import java.nio.file.*;
 import java.io.IOException;
 
 public class ReadFile {
-    static List<Integer> vogais = new ArrayList<>();
+    static List<Integer> vogais = new ArrayList<>(List.of(0, 0, 0, 0, 0));
     static int totalWords = 0;
+
     public static void main(String[] args) {
         try {
-            vogais.add(0);
-            vogais.add(0);
-            vogais.add(0);
-            vogais.add(0);
-            vogais.add(0);
             List<String> lines = readLines("atividade_7/strings.txt");
             int qtdLines = lines.size();
             int lineExistsCAO = -1;
-            List<String> words;
-            Object[] longestWord = {"", 0};
+            String[] words;
+            Object[] longestWord = { "", 0 };
             for (var i = 0; i < qtdLines; i++) {
-                words = List.of(lines.get(i).split(" "));
+                words = lines.get(i).split(" ");
                 for (String word : words) {
                     countVowelsAndWords(word);
                     if (word.length() > Integer.parseInt(longestWord[1].toString())) {
@@ -38,7 +34,7 @@ public class ReadFile {
             System.out.printf("Vogal mais recorrente: %s%n", recurrentVowel());
             System.out.printf("1º Ocorrência de 'ção' é na %d linha%n", lineExistsCAO);
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
@@ -60,8 +56,7 @@ public class ReadFile {
     }
 
     static String recurrentVowel() {
-        var biggest = Collections.max(vogais);
-        var index = vogais.indexOf(biggest);
+        var index = vogais.indexOf(Collections.max(vogais));
         return switch (index) {
             case 0 -> "a";
             case 1 -> "e";
