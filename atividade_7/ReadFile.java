69f0f45833e8609368cabf807e5086f4810121f0
Luiz Vinícius <viniciusadr244@gmail.com>
2024-03-08 08:05:05 -0300

    transform to pure functions

--- atividade_7/ReadFile.java
+++ atividade_7/ReadFile.java
@@ -5,33 +5,30 @@ import java.nio.file.*;
 import java.io.IOException;
 
 public class ReadFile {
-    static List<Integer> vogais = new ArrayList<>(List.of(0, 0, 0, 0, 0));
-    static int totalWords = 0;
-
     public static void main(String[] args) {
         try {
+            List<Integer> vogais = new ArrayList<>(List.of(0, 0, 0, 0, 0));
             List<String> lines = readLines("atividade_7/strings.txt");
-            int qtdLines = lines.size();
             int lineExistsCAO = -1;
             String[] words;
             Object[] longestWord = { "", 0 };
-            for (var i = 0; i < qtdLines; i++) {
+            int totalWords = 0;
+            for (int i = 0; i < lines.size(); i++) {
                 words = lines.get(i).split(" ");
                 for (String word : words) {
-                    countVowelsAndWords(word);
+                    totalWords = countVowelsAndWords(vogais, word);
                     if (word.length() > Integer.parseInt(longestWord[1].toString())) {
                         longestWord[0] = word;
                         longestWord[1] = word.length();
                     }
-                    boolean exists = existsCAO(word);
-                    if (lineExistsCAO == -1 && exists) {
+                    if (lineExistsCAO == -1 && existsCAO(word)) {
                         lineExistsCAO = i + 1;
                     }
                 }
             }
             System.out.printf("Existem %d palavras no texto.%n", totalWords);
             System.out.printf("A palavra %s possui %d caracteres é a mais longa%n", longestWord[0], longestWord[1]);
-            System.out.printf("Vogal mais recorrente: %s%n", recurrentVowel());
+            System.out.printf("Vogal mais recorrente: %s%n", recurrentVowel(vogais));
             System.out.printf("1º Ocorrência de 'ção' é na %d linha%n", lineExistsCAO);
         } catch (Exception e) {
             e.printStackTrace();
@@ -42,20 +39,21 @@ public class ReadFile {
         return Files.readAllLines(Paths.get(path));
     }
 
-    static void countVowelsAndWords(String word) {
+    static int countVowelsAndWords(List<Integer> vogais, String word) {
+        int totalWords = 0;
         word.toLowerCase().chars()
         .forEach(c -> {
             var index = "aeiou".indexOf(c);
             if (index != -1) vogais.set(index, vogais.get(index) + 1);
         });
-        totalWords++;
+        return totalWords++;
     }
 
     static Boolean existsCAO(String word) {
         return word.toLowerCase().contains("ção");
     }
 
-    static String recurrentVowel() {
+    static String recurrentVowel(List<Integer> vogais) {
         var index = vogais.indexOf(Collections.max(vogais));
         return switch (index) {
             case 0 -> "a";
