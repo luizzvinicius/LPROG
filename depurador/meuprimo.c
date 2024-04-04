97fbeb61d46003c15d73813893bebfc7e8b18314
Luiz Vinicius <viniciusadr244@gmail.com>
2024-04-04 08:57:51 -0300

    format c

--- depurador/meuprimo.c
+++ depurador/meuprimo.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 // Função para verificar se um número é primo
-int verificar_primo(int num) {
+int verificar_primo(int num)
+{
     if (num <= 1)
         return 0; // Não é primo
-    for (int i = 2; i * i <= num; i++) {
+    for (int i = 2; i * i <= num; i++)
+    {
         if (num % i == 0)
             return 0; // Não é primo
     }
@@ -12,20 +14,22 @@ int verificar_primo(int num) {
 }
 
 // Função para encontrar o próximo número primo após um número dado
-int proximo_primo(int num) {
+int proximo_primo(int num)
+{
     num++;
     while (!verificar_primo(num))
         num++;
     return num;
 }
 
-int main() {
+int main()
+{
     int n;
     printf("Digite um numero inteiro: ");
     scanf("%d", &n);
-    
+
     int primo = proximo_primo(n);
     printf("O menor numero primo maior que %d eh: %d\n", n, primo);
-    
+
     return 0;
 }
